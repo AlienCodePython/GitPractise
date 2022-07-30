a6cf1c2f366625d8ea32a0817e6451c65980613a
MeenaJeer <meenajeer@gmail.com>
2022-07-30 11:36:23 -0500

    made changes

---
+++ PythonFiles/DataTypes.py
@@ -0,0 +1,20 @@
+x = str("Hello World")
+
+#display x:
+print(x)
+
+#display the data type of x:
+print(type(x))
+
+y = 20
+print(y)
+print(type(y))
+z = 10.5
+print(z)
+print(type(z))
+b = True
+print(type(b))
+print(b)
+c=1+2j
+print(c)
+print(type(c)) 
