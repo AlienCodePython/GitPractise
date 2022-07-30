dbcad2e6fa2e7d04b08a60caeacdb89ee71fb4d3
Anil K Rajamoni <anil.k.rajamoni99@gmail.com>
2022-07-30 22:43:25 +0530

    added one java & javascript

---
+++ JavaFiles/Inheritance.java
@@ -0,0 +1,12 @@
+class A{
+    void print(){
+        System.out.println("Inside A class");
+    }
+}
+public class Inheritance{
+    public static void main(String[] args){
+
+        A a = new A();
+        a.print();
+    }
+}
