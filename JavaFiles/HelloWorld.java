8529b945148c6df7d7ae964ba55500eabe78ba80
Anil K Rajamoni <anil.k.rajamoni99@gmail.com>
2022-07-30 22:23:18 +0530

    added python files

---
+++ JavaFiles/HelloWorld.java
@@ -0,0 +1,10 @@
+public class HelloWorld{
+    public static void main(String[] args){
+        String name = "Java Repo";
+
+        System.out.println("Hello World");
+        System.out.println(name);
+
+
+    }
+}
